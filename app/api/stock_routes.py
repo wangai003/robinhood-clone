e9f88017910ed0536ef6145ae409e7d2eae53bb1
Eric Huang <huangeric.j@gmail.com>
2022-03-07 12:00:02 -0800

    Switched from iexcloud api to finnhub api
    Added stock quote and candles API routes

--- app/api/stock_routes.py
+++ app/api/stock_routes.py
@@ -1,6 +1,10 @@
+from ntpath import join
 import requests
 from os import environ
 from flask import Blueprint, jsonify, request
+import time
+import datetime
+import pytz
 
 stock_routes = Blueprint('stocks', __name__)
 
@@ -8,12 +12,9 @@ IN_PRODUCTION = environ.get('FLASK_ENV') == 'production'
 
 API_KEY = environ.get('API_KEY')
 SANDBOX_API_KEY = environ.get('SANDBOX_API_KEY')
+TOKEN = API_KEY if IN_PRODUCTION else SANDBOX_API_KEY
 
-API_URL = 'https://cloud.iexapis.com/stable'
-SANDBOX_API_URL = 'https://sandbox.iexapis.com/stable'
-
-KEY = API_KEY if IN_PRODUCTION else SANDBOX_API_KEY
-BASE_URL = API_URL if IN_PRODUCTION else SANDBOX_API_URL
+BASE_URL = 'https://finnhub.io/api/v1'
 
 
 @stock_routes.route('/')
@@ -34,61 +35,88 @@ def stock_symbols():
     return jsonify(data)
 
 
-@stock_routes.route('/<symbol>/company')
-def stock_company(symbol):
-    info = requests.get(
-        f'{BASE_URL}/stock/{symbol}/company', params={'token': KEY}
-    ).json()
-
-    data = {
-        'symbol': info['symbol'],
-        'name': info['companyName'],
-        'CEO': info['CEO'],
-        'employees': info['employees'],
-        'headquarters': f"{info['city']}, {info['country']}",
-    }
-
-    return jsonify(data)
-
-
 @stock_routes.route('/<symbol>/quote')
 def stock_quote(symbol):
-    quote = requests.get(
-        f'{BASE_URL}/stock/{symbol}/quote', params={'token': KEY}
-    ).json()
+    symbol = symbol.upper()
+    params = {
+        'symbol': symbol,
+        'token': TOKEN
+    }
+    res = requests.get(f'{BASE_URL}/quote', params=params).json()
 
     data = {
-        'open': quote['open'],
-        'close': quote['close'],
-        'change': quote['change'],
-        'change_percent': quote['changePercent'],
-        'high': quote['high'],
-        'low': quote['low'],
-        'week52_high': quote['week52High'],
-        'week52_low': quote['week52Low'],
-        'average_total_volume': quote['avgTotalVolume'],
-        'total_volume': quote['volume']
+        'current': res['c'],
+        'change': res['d'],
+        'change_percent': res['dp'],
+        'open': res['o'],
+        'previous_close': res['pc'],
+        'high': res['h'],
+        'low': res['l'],
     }
 
     return jsonify(data)
 
 
-@stock_routes.route('/<symbol>/news')
-def stock_news(symbol):
-    count = request.args.get('count') or 1
-    latest_news = requests.get(
-        f'{BASE_URL}/stock/{symbol}/news/last/{count}', params={'token': KEY}
-    ).json()
+@stock_routes.route('/<symbol>/candles')
+def stock_candles(symbol):
+    symbol = symbol.upper()
 
-    data = [
-        {
-            'datetime': news['datetime'],
-            'headline': news['headline'],
-            'summary': news['summary'],
-            'url': news['url'],
-            'image': news['image']
-        }
-        for news in latest_news
-    ]
+    # Open pre-hours for Robinhood at 9AM EST in unix timestamp
+    open = int(pytz.timezone('America/New_York').localize(datetime.datetime.today().replace(
+        hour=9, minute=0, second=0)).timestamp())
+
+    # Close after-hours for Robinhood at 4PM EST in unix timestamp
+    close = int(pytz.timezone('America/New_York').localize(datetime.datetime.today().replace(
+        hour=18, minute=0, second=0)).timestamp())
+
+    params = {
+        'symbol': symbol,
+        'resolution': request.args.get('resolution') or 5,
+        'from': request.args.get('from') or open,
+        'to': request.args.get('to') or close,
+        'token': TOKEN
+    }
+
+    res = requests.get(f'{BASE_URL}/stock/candle', params=params).json()
+
+    data = [{'time': t, 'price': c} for t, c in zip(res['t'], res['c'])]
 
     return jsonify(data)
+
+
+# @stock_routes.route('/<symbol>/company')
+# def stock_company(symbol):
+#     info = requests.get(
+#         f'{BASE_URL}/stock/{symbol}/company', params={'token': KEY}
+#     ).json()
+
+#     data = {
+#         'symbol': info['symbol'],
+#         'name': info['companyName'],
+#         'CEO': info['CEO'],
+#         'employees': info['employees'],
+#         'headquarters': f"{info['city']}, {info['country']}",
+#     }
+
+#     return jsonify(data)
+
+
+# @stock_routes.route('/<symbol>/news')
+# def stock_news(symbol):
+#     count = request.args.get('count') or 1
+#     latest_news = requests.get(
+#         f'{BASE_URL}/stock/{symbol}/news/last/{count}', params={'token': KEY}
+#     ).json()
+
+#     data = [
+#         {
+#             'datetime': news['datetime'],
+#             'headline': news['headline'],
+#             'summary': news['summary'],
+#             'url': news['url'],
+#             'image': news['image']
+#         }
+#         for news in latest_news
+#     ]
+
+#     return jsonify(data)
