b47ed4c9dbd3ab203680e0f17b79874ce672e277
Eric Huang <huangeric.j@gmail.com>
2022-03-08 13:06:24 -0800

    Profile menu styling

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,27 +1,42 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { logout } from '../../store/session';
 import './ProfileButton.css';
 
 const DropDownMenu = () => {
   const dispatch = useDispatch();
-  const history = useHistory();
   const user = useSelector(state => state.session.user);
 
   const onLogout = async e => {
     await dispatch(logout());
-    history.push('/splash');
   };
 
   return (
     <div className='profileMenu' onClick={e => e.stopPropagation()}>
-      <p>
-        {user.first_name} {user.last_name}
-      </p>
-      <button className='btn' onClick={onLogout}>
-        Logout
-      </button>
+      <div className='header'>
+        <h3 className='name'>
+          {user.first_name} {user.last_name}
+        </h3>
+        <div className='details'>
+          <div className='portfolioValue'>
+            <p className='value'>Some Value</p>
+            <p className='description'>Portfolio Value</p>
+          </div>
+          <div className='buyingPower'>
+            <p className='value'>Some Value</p>
+            <p className='description'>Buying Power</p>
+          </div>
+        </div>
+      </div>
+      <div className='body'>
+        <div className='menu'>stuff</div>
+      </div>
+      <div className='footer'>
+        <Link className='menu logout' onClick={onLogout} to='/login'>
+          Log Out
+        </Link>
+      </div>
     </div>
   );
 };
