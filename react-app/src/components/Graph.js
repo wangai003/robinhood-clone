0d66fb585a8b5b106cb9afa5ac110e73b3baaad5
pierikm <pierikm@umich.edu>
2022-03-08 20:06:36 -0500

    chart styling

--- react-app/src/components/Graph.js
+++ react-app/src/components/Graph.js
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-import { Line } from "react-chartjs-2";
+import { Line, getElementAtEvent } from "react-chartjs-2";
 import { Chart as ChartJS } from 'chart.js/auto'
 
 import './Graph.css'
 
 
 function Graph({ times, prices, color }) {
+    const chartRef = useRef();
 
     const data = {
         labels: times,
@@ -16,7 +17,7 @@ function Graph({ times, prices, color }) {
                 data: prices,
                 pointStyle: "circle",
                 pointRadius: 0,
-                borderColor: color,
+                borderColor: color === 'green' ? "#00a004" : "red",
             }
         ]
     }
@@ -32,11 +33,25 @@ function Graph({ times, prices, color }) {
                 text: 'Chart.js Line Chart',
             },
             tooltip: {
-                mode: 'index',
+                mode: 'nearest',
                 intersect: false,
                 displayColors: false,
                 yAlign: "bottom",
-                caretPadding: 10
+                caretPadding: 10,
+                callbacks: {
+                    label: function (context) {
+                        let label = context.dataset.label || '';
+
+                        if (label) {
+                            label += ': ';
+                        }
+                        if (context.parsed.y !== null) {
+                            label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
+                        }
+                        return label;
+                    },
+
+                }
             }
         },
         scales: {
--- react-app/src/components/Stock.js
+++ react-app/src/components/Stock.js
@@ -8,6 +8,7 @@ import "./Stock.css"
 function Stock() {
     const [stock, setStock] = useState({});
     const [interval, setInterval] = useState("1D");
+    const [intervalLong, setIntervalLong] = useState("Today");
     const [resolution, setResolution] = useState("5");
     const [prices, setPrices] = useState([]);
     const [times, setTimes] = useState([]);
@@ -36,20 +37,20 @@ function Stock() {
     useEffect(() => {
         (async () => {
             const fromTo = getInterval();
-            console.log(resolution)
             const response = await fetch(`/api/stocks/${symbol}/candles?from=${fromTo[0]}&to=${fromTo[1]}&resolution=${resolution}`);
             const data = await response.json();
             const prices = [];
             const times = [];
+
             for (const obj of data) {
                 prices.push(obj.price.toFixed(2));
                 times.push(convertTimes(obj.time));
             }
+
             const change = (prices[prices.length - 1] - prices[0]).toFixed(2);
             const changePercent = (change / prices[0]).toFixed(2);
             const color = change > 0 ? "green" : "red";
 
-            console.log(times);
             setChange(change)
             setChangePercent(changePercent)
             setColor(color);
@@ -62,19 +63,30 @@ function Stock() {
         console.log(int)
         switch (int) {
             case ('1D'):
+                setIntervalLong("Today");
                 setResolution("5");
+                break;
             case ('1W'):
+                setIntervalLong("Past Week");
                 setResolution("30");
+                break;
             case ('1M'):
+                setIntervalLong("Past Month");
                 setResolution("60");
+                break;
             case ('3M'):
+                setIntervalLong("Past 3 Months");
                 setResolution("D");
+                break;
             case ('1Y'):
+                setIntervalLong("Past Year");
                 setResolution("D");
-            case ('5Y'):
-                setResolution("W");
+                break;
+            // case ('5Y'):
+            //     setResolution("W");
+            //     break;
             default:
-
+                break;
         }
         setInterval(int);
     }
@@ -84,35 +96,26 @@ function Stock() {
         const year = date.getFullYear();
         const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date)
         const day = date.getDate();
-        // const hours = date.getHours() > 12 ? `${date.getHours() - 12}` : `${date.getHours()}`;
-        // const minutes = date.getMinutes() > 9 ? `${date.getMinutes()}` : `0${date.getMinutes()}`;
-        // const amPM = date.getHours() > 12 ? "PM" : "AM"
+        const hours = date.getHours() > 12 ? `${date.getHours() - 12}` : `${date.getHours()}`;
+        const minutes = date.getMinutes() > 9 ? `${date.getMinutes()}` : `0${date.getMinutes()}`;
+        const amPM = date.getHours() > 12 ? "PM" : "AM"
 
         switch (interval) {
             case ('1D'): {
-                const hours = date.getHours() > 12 ? `${date.getHours() - 12}` : `${date.getHours()}`;
-                const minutes = date.getMinutes() > 9 ? `${date.getMinutes()}` : `0${date.getMinutes()}`;
-                const amPM = date.getHours() > 12 ? "PM" : "AM"
                 return `${hours}:${minutes} ${amPM}`
             }
             case ('1W'): {
-                const hours = date.getHours() > 12 ? `${date.getHours() - 12}` : `${date.getHours()}`;
-                const minutes = date.getMinutes() > 9 ? `${date.getMinutes()}` : `0${date.getMinutes()}`;
-                const amPM = date.getHours() > 12 ? "PM" : "AM"
                 return `${month} ${day} ${hours}:${minutes} ${amPM}`;
             }
             case ('1M'): {
-                const hours = date.getHours() > 12 ? `${date.getHours() - 12}` : `${date.getHours()}`;
-                const minutes = date.getMinutes() > 9 ? `${date.getMinutes()}` : `0${date.getMinutes()}`;
-                const amPM = date.getHours() > 12 ? "PM" : "AM"
                 return `${month} ${day} ${hours}:${minutes} ${amPM}`;
             }
             case ('3M'):
                 return `${month} ${day}, ${year}`;
             case ('1Y'):
                 return `${month} ${day}, ${year}`;
-            case ('5Y'):
-                return `${month} ${day}, ${year}`;
+            // case ('5Y'):
+            //     return `${month} ${day}, ${year}`;
             default:
                 return '';
 
@@ -134,7 +137,7 @@ function Stock() {
         today.setMinutes(0);
         today.setSeconds(0);
         today.setMilliseconds(0);
-        const open = new Date(today - 28800000);
+        const open = new Date(today - 23400000);
         const oneDay = 86400000;
         const oneWeekAgo = new Date(today - oneDay * 7);
         const oneMonthAgo = new Date(today - oneDay * 30);
@@ -153,27 +156,29 @@ function Stock() {
                 return unixifyDates([threeMonthsAgo, today]);
             case ('1Y'):
                 return unixifyDates([oneYearAgo, today]);
-            case ('5Y'):
-                return unixifyDates([fiveYearsAgo, today]);
+            // case ('5Y'):
+            //     return unixifyDates([fiveYearsAgo, today]);
             default:
                 return ['', '']
         }
     }
 
     return (
-        <>
-            <div>
-                <div>
-                    <h2>{symbol}</h2>
-                    <h2>{`$${stock.current}`}</h2>
-                    <div>{`$${change} (${changePercent}%)`}</div>
+        <div className='stock-page-container'>
+            <div className='stock-container'>
+                <div className='stock-symbol-price-container'>
+                    <h2 id="stock-symbol">{symbol}</h2>
+                    <h2 id="stock-current-price">{`$${stock.current}`}</h2>
+                    <div id="stock-price-change">{change > 0 ? `$${change} (${changePercent}%)` : `-$${change * -1} (${changePercent}%)`}
+                        <div className='interval-long'>{intervalLong}</div>
+                    </div>
                 </div>
                 <Graph
                     symbol={symbol}
                     color={color}
                     times={times}
                     prices={prices} />
-                <nav>
+                <nav className='interval-bar'>
                     <button
                         className={interval === '1D' ? `interval-btn selected ${color}` : `interval-btn ${color}`}
                         onClick={(e) => handleClick(e.target.innerHTML)}>
@@ -199,25 +204,27 @@ function Stock() {
                         onClick={(e) => handleClick(e.target.innerHTML)}>
                         1Y
                     </button>
-                    <button
+                    {/* <button
                         className={interval === '5Y' ? `interval-btn selected ${color}` : `interval-btn ${color}`}
                         onClick={(e) => handleClick(e.target.innerHTML)}>
                         5Y
-                    </button>
+                    </button> */}
                 </nav>
-                <div>
+                <div className='stock-about-container'>
                     <h3>About</h3>
                     <p>This company is this</p>
                 </div>
             </div>
             <button
+                id='add-to-list-btn'
+                className={`${color}`}
                 onClick={() => setShowWatchlistform((prevState) => prevState === '' ? 'hide' : '')}>
-                Add to Watchlist
+                Add to Lists
             </button>
-            <div className={showWatchlistForm}>
+            <div className={`${showWatchlistForm} add-to-watchlist-form`}>
                 <AddToWatchlist hideForm={hideForm} symbol={symbol} />
             </div>
-        </>
+        </div>
     )
 }
 
