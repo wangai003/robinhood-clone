6a75f8091abe224e1ef718d6bfbd8d2b36e1f92c
pierikm <pierikm@umich.edu>
2022-03-09 14:02:36 -0500

    stock page financials and modal styled

--- react-app/src/components/Stock/AddToWatchlist.js
+++ react-app/src/components/Stock/AddToWatchlist.js
@@ -1,29 +1,38 @@
 import React, { useState } from 'react';
+import './AddToWatchlist.css'
 
 function AddToWatchlist({ hideForm, symbol, stock }) {
     const [currWatchlist, setCurrWatchlist] = useState('')
     // const watchlists = useSelector(state => state.watchlists)
     const watchlists = [{ name: "crypto", id: 1 }, { name: "tech", id: 2 }]
 
-    const submit = () => {
+    const submit = (e) => {
+        e.preventDefault();
         const errors = [];
         if (currWatchlist === '') {
             errors.push("Please select a watchlist")
         }
-        const payload = {
-            id: stock.id,
-            name: stock.name,
-            symbol: stock.symbol
-        }
+        // const payload = {
+        //     id: stock.id,
+        //     name: stock.name,
+        //     symbol: stock.symbol
+        // }
         if (!errors.length) {
+            hideForm();
             //add stock to watchlist
         }
     }
 
     return (
-        <div>
-            <form onSubmit={() => submit}>
+        <div className='stock-add-to-watchlist-container'>
+            <button
+                id="stock-add-to-watchlist-x"
+                onClick={() => hideForm()}>
+                X
+            </button>
+            <form onSubmit={(e) => submit(e)}>
                 <select
+                    id="stock-add-to-watchlist-select"
                     value={currWatchlist}
                     onChange={(e) => setCurrWatchlist(e.target.value)}>
                     <option value={''}>Select a Watchlist</option>
@@ -31,7 +40,7 @@ function AddToWatchlist({ hideForm, symbol, stock }) {
                         <option key={watchlist.id}>{watchlist.name}</option>
                     ))}
                 </select>
-                <button>Submit</button>
+                <button id="stock-add-to-watchlist-submit">Submit</button>
             </form>
         </div>
     )
