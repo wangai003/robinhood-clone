4c3918b4818bb351ecee9e9dd809119d3044ac95
Eric Huang <huangeric.j@gmail.com>
2022-03-11 08:21:42 -0800

    Moved portfolio loading to main wrapper

--- react-app/src/components/MainWrapper.js
+++ react-app/src/components/MainWrapper.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route } from 'react-router-dom';
 import NavBar from './Navigation';
+import { loadPortfolio } from '../store/portfolio';
 
 const MainWrapper = props => {
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(loadPortfolio());
+
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
   return (
-    <main>
-      <div className='content'>
-        <Route {...props}>
-          <NavBar user={props.user} />
-          {props.children}
-        </Route>
-      </div>
-    </main>
+    loaded && (
+      <main>
+        <div className='content'>
+          <Route {...props}>
+            <NavBar user={props.user} />
+            {props.children}
+          </Route>
+        </div>
+      </main>
+    )
   );
 };
 
