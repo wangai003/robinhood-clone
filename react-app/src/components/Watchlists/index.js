2bd8ea45e975d6b1d47d77769d650c557583ea32
pierikm <pierikm@umich.edu>
2022-03-13 16:21:22 -0400

    comment out hamburger

--- react-app/src/components/Watchlists/index.js
+++ react-app/src/components/Watchlists/index.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CreateWatchlistForm from '../CreateWatchlistform';
 import Watchlist from './Watchlist';
 import { deleteStockFromWatchlist, deleteWatchlistReducer } from '../../store/portfolio/watchlist';
 import 'react-tabs/style/react-tabs.css';
 import './Watchlist.css'
 import EditWatchlistForm from '../EditWatchlistForm';
-import Hamburger from 'hamburger-react'
+// import Hamburger from 'hamburger-react'
 import WatchlistMenu from './watchlistMenu';
 
 function WatchlistList() {
 
   const watchlists = Object.values(useSelector(state => state.portfolio.watchlists));
   const [showModal, setShowModal] = useState(false);
-  const [showEditModal,setShowEditModal] = useState(false)
+  const [showEditModal, setShowEditModal] = useState(false)
   const [showCreateWatchlistForm, changeCreateWatchlistForm] = useState(true);
   const [createWatchlistText, setCreateWatchlistText] = useState('New Watchlist');
-  const [activeEditWatchlist,setActiveEditWatchlist] = useState(null)
-  const [activeMenu,setActiveMenu] = useState(null)
+  const [activeEditWatchlist, setActiveEditWatchlist] = useState(null)
+  const [activeMenu, setActiveMenu] = useState(null)
   const dispatch = useDispatch();
   const toggleCreateWatchlistForm = async e => {
     setShowModal(true)
@@ -25,75 +25,75 @@ function WatchlistList() {
   };
   const [isOpen, setOpen] = useState(false)
 
-  const toggleShowEditModal = (e ,watchlist) => {
+  const toggleShowEditModal = (e, watchlist) => {
     setShowEditModal(true)
     console.log()
     setActiveEditWatchlist(watchlist)
     console.log(watchlist)
   }
-  const deleteWatchlist = async (e,watchlist) => {
+  const deleteWatchlist = async (e, watchlist) => {
     await dispatch(deleteWatchlistReducer(watchlist.id));
-};
+  };
 
-const toggleIsOpen = (e) =>{
-  setOpen(!isOpen)
-}
-let editForm;
-let menu;
-if (showEditModal) {
-  editForm = (
-    <EditWatchlistForm
-      watchlist={activeEditWatchlist}
-      showModal = {showEditModal}
-      setShowModal = {setShowEditModal}
-    ></EditWatchlistForm>
+  const toggleIsOpen = (e) => {
+    setOpen(!isOpen)
+  }
+  let editForm;
+  let menu;
+  if (showEditModal) {
+    editForm = (
+      <EditWatchlistForm
+        watchlist={activeEditWatchlist}
+        showModal={showEditModal}
+        setShowModal={setShowEditModal}
+      ></EditWatchlistForm>
 
-  );
-}
-if(isOpen){
-  menu = <WatchlistMenu watchlist={activeEditWatchlist}></WatchlistMenu>
-}
+    );
+  }
+  if (isOpen) {
+    menu = <WatchlistMenu watchlist={activeEditWatchlist}></WatchlistMenu>
+  }
   return (
     <div className='watchlistListContainer'>
       <div>
-         <button className={"createWatchlistButton"} onClick={toggleCreateWatchlistForm}>{createWatchlistText}</button>
-      {showModal && (
-        <CreateWatchlistForm hideform={toggleCreateWatchlistForm}showModal={showModal} setShowModal={setShowModal}></CreateWatchlistForm>
-      )}
+        <button className={"createWatchlistButton"} onClick={toggleCreateWatchlistForm}>{createWatchlistText}</button>
+        {showModal && (
+          <CreateWatchlistForm hideform={toggleCreateWatchlistForm} showModal={showModal} setShowModal={setShowModal}></CreateWatchlistForm>
+        )}
       </div>
 
-        {watchlists &&
+      {watchlists &&
         watchlists.map(watchlist => {
           return <div
-           key={'watchlist'+watchlist.id} className={"notActiveWatchlist watchlist"} onClick= {(event) => {
-            if(event.target.tagName !== 'BUTTON' && event.target.tagName !== 'SPAN'){
-              //target is specific event.target === event.currentTarget ||
-              console.log("hitting true")
-              if(event.currentTarget.classList.contains("activeWatchlist"))
+            key={'watchlist' + watchlist.id} className={"notActiveWatchlist watchlist"} onClick={(event) => {
+              if (event.target.tagName !== 'BUTTON' && event.target.tagName !== 'SPAN') {
+                //target is specific event.target === event.currentTarget ||
+                console.log("hitting true")
+                if (event.currentTarget.classList.contains("activeWatchlist"))
                   event.currentTarget.classList.remove("activeWatchlist")
-              else{
-                event.currentTarget.classList.add("activeWatchlist")
+                else {
+                  event.currentTarget.classList.add("activeWatchlist")
+                }
+              } else {
+                //   console.log("fasle")
+                // console.log(event.target)
               }
-            } else{
-            //   console.log("fasle")
-            // console.log(event.target)
-            }
-          }}>
+            }}>
             <div className='flexDiv'>
 
-            {/* <Hamburger size={20} toggled={isOpen} toggle={setOpen}>
+              {/* <Hamburger size={20} toggled={isOpen} toggle={setOpen}>
              </Hamburger> */}
-             {/* all menus are opening rather than just the one clicked */}
-             {/* <div className='innerFlexDiv'>
+              {/* all menus are opening rather than just the one clicked */}
+              {/* <div className='innerFlexDiv'>
              <span onClick={(e) => toggleIsOpen(e)}> + </span>
              {isOpen &&<WatchlistMenu watchlist={watchlist}></WatchlistMenu>}
              </div> */}
-            {/* <button className='editWatchlist' onClick={(e)=> toggleShowEditModal(e,watchlist)}>Edit</button>
+              {/* <button className='editWatchlist' onClick={(e)=> toggleShowEditModal(e,watchlist)}>Edit</button>
             <button className='deleteWatchlist testing' onClick={(e) => deleteWatchlist(e,watchlist)}>Delete</button> */}
-             {/* {editForm} */}
-            {<Watchlist watchlist={watchlist}></Watchlist>}
+              {/* {editForm} */}
+              {<Watchlist watchlist={watchlist}></Watchlist>}
             </div>
-            </div>
+          </div>
         })}
     </div>
   );
